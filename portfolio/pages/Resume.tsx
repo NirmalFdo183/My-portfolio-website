93d6666ae171c11095d41e8adb4bdd84eca83228
Nirmal <fernandonirmal345@gmail.com>
2025-02-27 23:37:40 +0530

    recreating the portfolio website

--- portfolio/pages/Resume.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import NavBar from '/Users/nirmalfernando/Myprojects/My-portfolio-website/portfolio/components/NavBar.tsx'
-
-export const Resume = () => {
-  return (
-    <div><NavBar/></div>
-    
-  )
-}
--- portfolio/pages/Services.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import NavBar from '/Users/nirmalfernando/Myprojects/My-portfolio-website/portfolio/components/NavBar.tsx'
-
-export const Services = () => {
-  return (
-    <div><NavBar/></div>
-    
-  )
-}
